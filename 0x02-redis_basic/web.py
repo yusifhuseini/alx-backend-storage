21d784a60e866117cb634a2e332a15c959429bbc
yusifhuseini <yusifhuseini@gmail.com>
2024-05-16 05:42:53 +0000

    0x02. Redis basic

--- 0x02-redis_basic/web.py
+++ 0x02-redis_basic/web.py
@@ -1,24 +1,32 @@
 #!/usr/bin/env python3
-"""
-Defines a function `get_page`
-"""
+""" Redis Module """
+
+from functools import wraps
 import redis
 import requests
-from datetime import timedelta
+from typing import Callable
+
+redis_ = redis.Redis()
 
 
+def count_requests(method: Callable) -> Callable:
+    """ Decortator for counting """
+    @wraps(method)
+    def wrapper(url):  # sourcery skip: use-named-expression
+        """ Wrapper for decorator """
+        redis_.incr(f"count:{url}")
+        cached_html = redis_.get(f"cached:{url}")
+        if cached_html:
+            return cached_html.decode('utf-8')
+        html = method(url)
+        redis_.setex(f"cached:{url}", 10, html)
+        return html
+
+    return wrapper
+
+
+@count_requests
 def get_page(url: str) -> str:
-    """
-    It uses the requests module to obtain
-    the HTML content of a particular URL and returns it.
-    Args:
-        url (str): url whose content is to be fectched
-    Returns:
-        html (str): the HTML content of the url
-    """
-    r = redis.Redis()
-    key = "count:{}{}{}".format('{', url, '}')
-    r.incr(key)
-    res = requests.get(url)
-    r.setex(url, timedelta(seconds=10), res.text)
-    return res.text
+    """ Obtain the HTML content of a  URL """
+    req = requests.get(url)
+    return req.text
